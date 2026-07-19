5c9bd2add67b7fe04ae4b677bcc7ccf5d9fc16d1
sharmayush11 <ayusharma1127@gmail.com>
2026-07-19 19:36:26 +0530

    updated movieDetail and AppCss

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -20,15 +20,19 @@ function MovieDetail() {
 
   return (
     <div className="movie-detail">
-      <h2>{movie.Title}</h2>
-      <img alt={movie.Title} src={movie.Poster} />
-      <p><strong>Genre:</strong> {movie.Genre}</p>
-      <p><strong>Released:</strong> {movie.Released}</p>
-      <p><strong>Plot:</strong> {movie.Plot}</p>
-      <p><strong>Actors:</strong> {movie.Actors}</p>
-      <p><strong>Director:</strong> {movie.Director}</p>
-      <p><strong>IMDb Rating:</strong> {movie.imdbRating}</p>
-    </div>
+  <img src={movie.Poster} alt={movie.Title} />
+
+  <div className="movie-info">
+    <h2>{movie.Title}</h2>
+
+    <p><strong>Genre:</strong> {movie.Genre}</p>
+    <p><strong>Released:</strong> {movie.Released}</p>
+    <p><strong>Actors:</strong> {movie.Actors}</p>
+    <p><strong>Director:</strong> {movie.Director}</p>
+    <p><strong>IMDb Rating:</strong> {movie.imdbRating}</p>
+    <p><strong>Plot:</strong> {movie.Plot}</p>
+  </div>
+</div>
   );
 }
 
